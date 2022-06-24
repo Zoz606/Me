0bd84df3daf0d1765314bff0a91efb36df870602
Zoz606 <87106742+Zoz606@users.noreply.github.com>
2022-06-25 01:30:38 +0200

    ,

--- interface/first/main.c
+++ interface/first/main.c
@@ -1,13 +1,12 @@
-#define F_CPU 8000000UL
 #include <util/delay.h>
 #include <avr/io.h>
 #include "std_macros.h"
 
 int main(void) {
-	SET_BIT(DDRA, 1);
+	SET_BIT(DDRA, 0);
 	while(1) {
-		PORTA ^= (1<<2);
-		_delay_ms(1000);
+		TOG_BIT(PORTA, 0);
+		_delay_ms(2000);
 		}
 
 }
